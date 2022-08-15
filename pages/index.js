144a375c4b8ff7a83c60c5683a54193d21b59d92
Hoggy06 <speedrun.fra@gmail.com>
2022-08-15 10:44:59 +0200

    suppression de nav et footer

--- pages/index.js
+++ pages/index.js
@@ -10,17 +10,13 @@ import About from "../components/About";
 import Projects from "../components/Projects";
 import Career from "../components/Career";
 import Contact from "../components/Contact/Contact";
-import Nav from "../components/Base/Nav";
-import Footer from "../components/Base/Footer";
 import "animate.css/animate.min.css";
 import { AnimationOnScroll } from "react-animation-on-scroll";
 import { LanguageContext } from "./_app";
 export default function Home({ data }) {
-  const { language, toogleLanguage } = useContext(LanguageContext);
+  const { language } = useContext(LanguageContext);
   return (
     <Fragment>
-      <Nav data={data} language={language} toogleLanguage={toogleLanguage} />
-
       <Intro data={data} language={language} />
 
       <About data={data} language={language} />
@@ -30,8 +26,6 @@ export default function Home({ data }) {
       <Career data={data} language={language} />
 
       <Contact data={data} language={language} />
-
-      <Footer data={data} language={language} />
     </Fragment>
   );
 }
