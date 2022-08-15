95749a383d70174f85a07d31dc9100852110d409
Hoggy06 <speedrun.fra@gmail.com>
2022-08-15 10:44:33 +0200

    ajout de variables .env

--- next.config.js
+++ next.config.js
@@ -7,4 +7,10 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  env: {
+    DOMAIN_NAME: "http://localhost:3005",
+    EMAIL_PROVIDER: "smtp.orange.fr",
+    EMAIL_ADDRESS: "tourniermaxime@orange.fr",
+    EMAIL_PASSWORD: "s45o1qAHdVeJL419zy6aP0WI",
+  },
 };
