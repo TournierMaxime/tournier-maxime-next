c92dd1d9d9ab8c221955eee0fe460ad17f321aa5
Hoggy06 <speedrun.fra@gmail.com>
2022-08-15 14:17:05 +0200

    ajout du favicon

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600;700;900&display=swap"
           rel="stylesheet"
         />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <meta description="Passionné par les nouvelles technologies. Mes premières expériences en tant que développeur remontent à 2008. Je me suis formé en autodidacte sur le 'site du zéro' qui est devenu OpenClassRooms. D'abord en apprenant HTML et CSS. Puis c'est en 2016 que j'ai appris PHP et MySQL. Cet apprentissage m'a permis de créer mon premier site internet personnel. En 2021 j'ai décidé de me former pour devenir développeur/intégrateur web notamment sur Javascript, ReactJS et NodeJS." />
       </Head>
       <body className="body">
