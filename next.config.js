477b194b48c467a2cb04cf512ab6920082c1d6f0
Hoggy06 <speedrun.fra@gmail.com>
2022-08-15 10:50:35 +0200

    changement du nom de domaine

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = {
     includePaths: [path.join(__dirname, "styles")],
   },
   env: {
-    DOMAIN_NAME: "http://localhost:3005",
+    DOMAIN_NAME: "https://tmnext.tourniermaxime.name",
     EMAIL_PROVIDER: "smtp.orange.fr",
     EMAIL_ADDRESS: "tourniermaxime@orange.fr",
     EMAIL_PASSWORD: "s45o1qAHdVeJL419zy6aP0WI",
